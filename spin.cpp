7c25a71b1bbe49f317cb5774fee2f0a12ce41c93
Palash Vijay <palashvijay13319@gmail.com>
2016-02-14 21:21:32 +0530

    added file names to vector

--- spin.cpp
+++ spin.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <boost/geometry.hpp>
 #include <boost/graph/adjacency_list.hpp>
-#include <string>
+#include <string.h>
 #include <dirent.h>
+#include <vector>
 using namespace std;
 int main()
 {
@@ -11,12 +12,22 @@ int main()
 	 */ 
 	DIR *dir;
 	struct dirent *ent;
+    vector <string> dirContents;
 	if ((dir = opendir ("Input")) != NULL) {
 		/* print all the files and directories within directory */
 		while ((ent = readdir (dir)) != NULL) {
-			printf ("%s\n", ent->d_name);
+			if(std::strcmp(ent->d_name,".") ==0)
+                ;
+            else if(std::strcmp(ent->d_name,"..") == 0)
+                ;
+            else
+                dirContents.push_back(ent->d_name);
 		}
-		closedir (dir);
+	closedir (dir);
+    std::vector<string>::iterator i;
+    for(i=dirContents.begin();i!=dirContents.end();i++)
+        cout << *i << " " ;
+    cout << endl;
 	} 
 	else {
 		/* could not open directory */
