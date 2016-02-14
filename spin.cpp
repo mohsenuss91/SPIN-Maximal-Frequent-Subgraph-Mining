3d709b9ee4ff6b40d2a48d1c8b05d77bcadda173
Palash Vijay <palashvijay13319@gmail.com>
2016-02-14 17:34:22 +0530

    getting files from input directory

--- spin.cpp
+++ spin.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <boost/geometry.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <string>
+#include <dirent.h>
 using namespace std;
 int main()
 {
 	/*
-	Basically all the other functions will be called from main.
-	*/ 
+	   Basically all the other functions will be called from main.
+	 */ 
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir ("Input")) != NULL) {
+		/* print all the files and directories within directory */
+		while ((ent = readdir (dir)) != NULL) {
+			printf ("%s\n", ent->d_name);
+		}
+		closedir (dir);
+	} 
+	else {
+		/* could not open directory */
+		perror ("");
+		return EXIT_FAILURE;
+	}
 	return 0;
 }
